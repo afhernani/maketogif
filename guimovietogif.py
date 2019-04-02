82c8ec86a649b5868dc54337f8dcf73c4a5cf4e8
hernani Aleman Ferraz <afhernani@hotmail.com>
2019-04-02 03:38:00 +0100

    creted class movie and threads intro its

--- guimovietogif.py
+++ guimovietogif.py
@@ -13,9 +13,191 @@ import shutil
 import configparser
 import threading
 
-thread = threading
-bloquea = threading.Lock()
-isthread = False
+
+class Movie:
+    def __init__(self, file):
+        self.exists = False
+        self.file=" "
+        self.path_file=" "
+        self.datos = {'time': 0, 'fps': 1, 'width': 1, 'height': 1, 'bitrate': 1, 'num': 1 }
+        if os.path.exists(file):
+            self.exists = True
+            self.file = os.path.split(file)[-1]
+            self.path_file = os.path.dirname(file)
+            self.datos['file'] = self.file
+            self.datos[ 'path_file'] = self.path_file
+            self.datos['exists'] = self.exists
+
+
+    def __str__(self):
+        return str(self.datos)
+
+    def printOutput(self, string):
+        '''
+        Pretty print multi-line string
+        '''
+        for line in string.splitlines():
+            print('    >> {}'.format(line.decode('utf8')))
+
+    def runCommand(self, command):
+        p = subprocess.Popen(command, shell=False, stdout=subprocess.PIPE, stderr=subprocess.STDOUT).stdout
+        while True and p:
+            line = p.readline()
+            if not line:
+                break
+            self.printOutput(line)
+
+    def info_from_video(self):
+        print('extrac_informacion ->', self.datos['file'])
+        if not self.datos['exists']:
+            print('no se pudo extraer informacion del fichero')
+            return
+        movie = os.path.join(self.datos['path_file'], self.datos['file'])
+        command = ['ffmpeg', '-i', movie]
+        try:
+            process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+            stdout, stderr = process.communicate()
+
+            matches = re.search(b'Duration:\s{1}(?P<hours>\d+?):(?P<minutes>\d+?):(?P<seconds>\d+\.\d+?),',
+                                stdout, re.DOTALL).groupdict()
+            if matches:
+                t_hour = matches['hours']
+                t_min = matches['minutes']
+                t_sec = matches['seconds']
+
+                t_hour_sec = int(t_hour) * 3600
+                t_min_sec = int(t_min) * 60
+                t_s_sec = float(t_sec)
+
+                total_sec = t_hour_sec + t_min_sec + t_s_sec
+                self.datos['time'] = total_sec
+                print('hora, minuto, segundo:', t_hour, t_min, t_sec)
+                print('total de segundos:', total_sec)
+
+            # This matches1 is to get the frames por segundo
+            matches1 = re.search(b'(?P<fps>(\d+.)* fps)', stdout)
+            if matches1:
+                print('fps ->', matches1['fps'])
+                frame_rate = matches1['fps'].split(b' ')[0]
+                self.datos['fps'] = float(frame_rate)
+                print('fps ->', self.datos['fps'] )
+
+            matches2 = re.search(b' (?P<width>\d+)x(?P<height>\d{2,3}[, ])', stdout)
+            if matches2:
+                print(matches2)
+                width = int(matches2['width'])
+                height = int(matches2['height'].replace(b',',b' '))
+                print('formato: ->', matches2, width, height)
+                self.datos['width'] = width
+                self.datos['height'] = height
+
+
+            #averiaguar el ratio.
+            matches3 = re.search(b'bitrate:\s{1}(?P<bitrate>\d+?)\s{1}', stdout)
+            if matches3:
+                print('matches3 ->', matches3['bitrate'])
+                self.datos['bitrate'] = int(matches3['bitrate'])
+
+        except subprocess.CalledProcessError as e:
+            print(e.output)
+        print('extrac informacion from ->', self.datos['file'])
+        print(self.datos)
+
+
+    def extract_frames(self, file=None, num=None):
+        '''
+        extract frames from file
+        file = file asignated, num = number of frames extract video to the gif
+        :return: None
+        '''
+        print('extract frames from ->', self.datos['file'])
+        if not self.datos['exists']:
+            print('no se pudo extraer imagenes del fichero', self.datos['file'])
+            return
+        if not file:
+            file = os.path.join(self.datos['path_file'], self.datos['file'])
+        # obtener un nombre de fichero independiente del fichero y fichero de trabajo
+        if not num:
+            num = self.datos['num']
+        import uuid
+        name = str(uuid.uuid4()) + '-%04d.png'
+        self.datos['code_frame'] = name
+        #working file:
+        working_file = os.path.join(self.datos['path_file'], 'Thumbails')
+        self.datos['working_file'] = working_file
+        if not os.path.exists(working_file):
+            os.mkdir(working_file)
+        # ficheros de frames de salida
+        work_dir = os.path.join(working_file, name)
+        #determinar los factores de la linea de comando.
+        command = ['ffmpeg']
+        valor = self.datos['time']
+        if valor:
+            valor = valor * 1000 / (num + 1)
+        command.extend(['-ss', str(int(valor/1000)), '-i', file, '-vf', 'fps=1/' + str(int(valor/1000))
+                           , '-vframes', str(num)
+                           ,  work_dir, '-hide_banner'])
+        print('Linea de comando ->', command)
+        self.runCommand(command)
+        print('end extract_frames from ->', self.datos['file'])
+        print(self.datos)
+
+
+    def make_gif_f_frames(self, framerate=1, scale='200:-1'):
+        '''
+        make a gif from cedec frame
+        :return:
+        '''
+        print('make gif from ->', self.datos['file'])
+        if not self.datos['exists']:
+            print('no se puedo crear el gif desde las imagenes de ->', self.datos['file'])
+            return
+        name = self.datos['code_frame']
+        working_file = self.datos['working_file']
+        work_dir = os.path.join(working_file, name)
+        command = ['ffmpeg', '-y', '-framerate', str(framerate), '-i', work_dir, '-vf', 'scale=' + scale ]
+        file_out = os.path.join(working_file, self.datos['file'] + '_thumbs_0000.gif')
+        command.extend([file_out])
+        print('create gif command ->', command)
+        self.runCommand(command)
+        #vamos a borrar los ficheros de imagen
+        code = name.split('-')[0]
+        pattern = '^' + code
+        mypath = self.datos['working_file']
+        print('remove:')
+        for root, dirs, files in os.walk(mypath):
+            for file in filter(lambda x: re.match(pattern, x), files):
+                print(file)
+                os.remove(os.path.join(root, file))
+        '''
+        code.pop()
+        refcode=''
+        for item in code:
+            refcode += item + '-'
+        print('remove:')
+        print(refcode + '*.png')
+        import fnmatch
+        for file in os.listdir(self.datos['working_file']):
+            print(file)
+            if fnmatch.fnmatch(file.upper(), refcode + '*.png'):
+                print(file)
+                os.remove(os.path.join(self.datos['working_file'], file))
+        '''
+        print('made gif  from ->', self.datos['file'])
+
+    def make_gif(self):
+        if self.datos['exists']:
+            self. info_from_video()
+            print('datos generales ->', self.datos)
+            self.extract_frames(num=5)
+            self.make_gif_f_frames()
+            print('datos generales ->', self.datos)
+
+    def run(self):
+        thread = threading.Thread(target=self.make_gif)
+        thread.daemon = True
+        thread.start()
+
 
 class GuiMovieToGif(tk.Tk):
     def __init__(self):
@@ -165,22 +347,16 @@ class GuiMovieToGif(tk.Tk):
             self.dirpathmovies.set(os.path.abspath(dirname))
             self.init_listbox(os.path.abspath(dirname))
 
-    def make_thread(self):
-        global bloquea
-        global isthread
-        bloquea.acquire()
-        if os.path.exists(self.file_select.get()):
-            self.datos = self.info_from_video()
-            print('datos video ->', self.datos)
-            self.extract_frames(num=5)
-            self.make_gif_f_frames()
-            print('datos video ->', self.datos)
-        bloquea.release()
-        isthread = False
-
     def make_gif(self):
         print('make gif instruction')
         self.statusvalor.set('make gif instruction')
+        if os.path.exists(self.file_select.get()):
+            movie = Movie(self.file_select.get())
+            print(movie)
+            movie.run()
+        else:
+            return
+        '''
         global thread
         global isthread
         if isthread:
@@ -190,6 +366,7 @@ class GuiMovieToGif(tk.Tk):
         thread.daemon = True
         thread.start()
         isthread = True
+        '''
 
     def confirmExit(self):
         if messagebox.askokcancel('Quit', 'Are you sure you want to exit?'):
@@ -207,147 +384,6 @@ class GuiMovieToGif(tk.Tk):
             if file.endswith(".flv"):
                 self.box_list.insert(tk.END, file)
         self.box_list.selection_set(0)
-
-    def printOutput(self, string):
-        '''
-        Pretty print multi-line string
-        '''
-        for line in string.splitlines():
-            print('    >> {}'.format(line.decode('utf8')))
-
-    def runCommand(self, command):
-        p = subprocess.Popen(command, shell=False, stdout=subprocess.PIPE, stderr=subprocess.STDOUT).stdout
-        while True and p:
-            line = p.readline()
-            if not line:
-                break
-            self.printOutput(line)
-
-    def info_from_video(self):
-        print('extrac_images_from_videos')
-        video_info = {}
-        video_info['movie'] = self.file_select.get()
-        command = ['ffmpeg', '-i', video_info['movie']]
-
-        try:
-            process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-            stdout, stderr = process.communicate()
-
-            matches = re.search(b'Duration:\s{1}(?P<hours>\d+?):(?P<minutes>\d+?):(?P<seconds>\d+\.\d+?),',
-                                stdout, re.DOTALL).groupdict()
-            if matches:
-                t_hour = matches['hours']
-                t_min = matches['minutes']
-                t_sec = matches['seconds']
-
-                t_hour_sec = int(t_hour) * 3600
-                t_min_sec = int(t_min) * 60
-                t_s_sec = float(t_sec)
-
-                total_sec = t_hour_sec + t_min_sec + t_s_sec
-                video_info['time']=total_sec
-                print('hora, minuto, segundo:', t_hour, t_min, t_sec)
-                print('total de segundos:', total_sec)
-
-            # This matches1 is to get the frames por segundo
-            matches1 = re.search(b'(?P<fps>(\d+.)* fps)', stdout)
-            if matches1:
-                print('fps ->', matches1['fps'])
-                frame_rate = matches1['fps'].split(b' ')[0]
-                video_info['fps'] = float(frame_rate)
-                print('fps ->', float(frame_rate))
-
-            matches2 = re.search(b' (?P<width>\d+)x(?P<heigh>\d{2,3}[, ])', stdout)
-            if matches2:
-                print(matches2)
-                width = int(matches2['width'])
-                heigh = int(matches2['heigh'].replace(b',',b' '))
-                print('formato: ->', matches2, width, heigh)
-                video_info['formato'] = [width, heigh]
-
-
-            #averiaguar el ratio.
-            matches3 = re.search(b'bitrate:\s{1}(?P<bitrate>\d+?)\s{1}', stdout)
-            if matches3:
-                print('matches3 ->', matches3['bitrate'])
-                video_info['bitrate'] = int(matches3['bitrate'])
-
-        except subprocess.CalledProcessError as e:
-            print(e.output)
-
-        return video_info
-
-    def extract_frames(self, file=None, num=None):
-        '''
-        extract frames from file
-        file = file asignated, num = number of frames extract video to the gif
-        :return: None
-        '''
-        if not file:
-            file = self.datos['movie']
-        # obtener un nombre de fichero independiente del fichero y fichero de trabajo
-        if not num:
-            num = 1
-        import uuid
-        name = str(uuid.uuid4()) + '-%04d.png'
-        self.datos['code_frame'] = name
-        #working file:
-        working_file = os.path.join(os.path.dirname(self.datos['movie']), 'Thumbails')
-        self.datos['working_file'] = working_file
-        if not os.path.exists(working_file):
-            os.mkdir(working_file)
-        # ficheros de frames de salida
-        work_dir = os.path.join(working_file, name)
-        #determinar los factores de la linea de comando.
-        command = ['ffmpeg']
-        valor = self.datos['time']
-        if valor:
-            valor = valor * 1000 / (num + 1)
-        command.extend(['-ss', str(int(valor/1000)), '-i', file, '-vf', 'fps=1/' + str(int(valor/1000))
-                           , '-vframes', str(num)
-                           ,  work_dir, '-hide_banner'])
-        print('Linea de comando ->', command)
-        self.runCommand(command)
-        print('end extract_frames')
-
-    def make_gif_f_frames(self, framerate=1, scale='200:-1'):
-        '''
-        make a gif from cedec frame
-        :return:
-        '''
-        name = self.datos['code_frame']
-        working_file = self.datos['working_file']
-        work_dir = os.path.join(working_file, name)
-        command = ['ffmpeg', '-y', '-framerate', str(framerate), '-i', work_dir, '-vf', 'scale=' + scale ]
-        self.datos['file']= os.path.split(self.datos['movie'])[-1]
-        file_out = os.path.join(os.path.dirname(self.datos['movie']), 'Thumbails', self.datos['file'] + '_thumbs_0000.gif')
-        command.extend([file_out])
-        print('create gif command ->', command)
-        self.runCommand(command)
-        #vamos a borrar los ficheros de imagen
-        code = name.split('-')[0]
-        pattern = '^' + code
-        mypath = self.datos['working_file']
-        print('remove:')
-        for root, dirs, files in os.walk(mypath):
-            for file in filter(lambda x: re.match(pattern, x), files):
-                print(file)
-                os.remove(os.path.join(root, file))
-        '''
-        code.pop()
-        refcode=''
-        for item in code:
-            refcode += item + '-'
-        print('remove:')
-        print(refcode + '*.png')
-        import fnmatch
-        for file in os.listdir(self.datos['working_file']):
-            print(file)
-            if fnmatch.fnmatch(file.upper(), refcode + '*.png'):
-                print(file)
-                os.remove(os.path.join(self.datos['working_file'], file))
-        '''
-        print('made_gif_f_frames')
 
 
 if __name__ == '__main__':
